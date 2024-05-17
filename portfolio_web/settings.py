ace19b2131f2f389419a90b5002992800b78334e
ddylvianen <dylchelvia@gmail.com>
2024-05-17 07:44:57 -1200

    sds

--- portfolio_web/settings.py
+++ portfolio_web/settings.py
@@ -15,7 +15,10 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+MEDIA_URL = "/media/"
+
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# or, eg,
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
