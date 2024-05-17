1b909457a42a7e350eb1f8ff891934e96200c1d5
ddylvianen <dylchelvia@gmail.com>
2024-05-17 10:50:27 -1200

    hh

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('mijn_werk/', views.mijn_werk, name='mijn_werk'),
     path('over_mij/', views.over_mij, name='over_mij'),
     path('services/', views.services, name='services'),
+    path('werk/<int:id>', views.werk_sing, name='solo-work'),
 ]
--- portfolio/views.py
+++ portfolio/views.py
@@ -14,12 +14,21 @@ def contact(request):
     return render(request, 'portfolio/contact.html')
 
 def mijn_werk(request):
-    werken = Projects.objects.order_by('id')
-    link = "/media/"
-    return render(request, 'portfolio/mijn_werk.html', {'werken': werken, 'link': link})
+    context = {
+        'werken': Projects.objects.order_by('id'), 
+        'link': "/media/"
+    }
+    return render(request, 'portfolio/mijn_werk.html', context)
 
 def over_mij(request):
     return render(request, 'portfolio/over_mij.html')
 
 def services(request):
     return render(request, 'portfolio/services.html')
+
+def werk_sing(request, id):
+    context = {
+        'werk': Projects.objects.get(id=id),
+        'link': "/media/",
+    }
+    return render(request, 'portfolio/werk_sing.html', context)
