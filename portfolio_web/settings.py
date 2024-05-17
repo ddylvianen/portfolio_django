def566237041dc8bcf37b3f5ae35730ac59bb89e
ddylvianen <dylchelvia@gmail.com>
2024-05-17 07:13:06 -1200

    h

--- portfolio_web/settings.py
+++ portfolio_web/settings.py
@@ -15,13 +15,9 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-
-STATIC_ROOT = "/home/myusername/myproject/static"
-# or, eg,
-STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
