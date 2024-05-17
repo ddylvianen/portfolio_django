cd671863ce582e534b9cc1fd97ceba779e7a4e70
ddylvianen <dylchelvia@gmail.com>
2024-05-17 10:14:45 -1200

    new

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,8 +4,10 @@ from django.utils import timezone
 # Create your models here.
 class Projects(models.Model):
    id = models.AutoField(primary_key=True)
-   title = models.CharField(max_length=200)
+   title = models.CharField(max_length=200, null=True)
+   description = models.CharField(max_length=200, null=True)
    img = models.ImageField(upload_to='images', default="default.png")
+   url = models.CharField(max_length=500, null=True)
    published_date = timezone.now()
    
    def publish(self):
